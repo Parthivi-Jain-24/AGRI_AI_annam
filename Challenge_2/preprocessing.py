125e9a686cdedae3ac0d8deeb562b50efac0bbd0
Parthivi-Jain-24 <161028902+Parthivi-Jain-24@users.noreply.github.com>
2025-05-25 16:09:18 +0530

    Create preprocessing.py

---
+++ Challenge_2/preprocessing.py
@@ -0,0 +1,6 @@
+# Preprocessing
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5]*3, [0.5]*3)  # Normalize to [-1, 1] range
+])
