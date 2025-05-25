5b2fc68abb4e864f2894338ba4a25a0a8aebfea6
Parthivi-Jain-24 <161028902+Parthivi-Jain-24@users.noreply.github.com>
2025-05-25 16:05:34 +0530

    Create preprocessing.py

---
+++ Challenge_1/src/preprocessing.py
@@ -0,0 +1,20 @@
+# Transformations
+image_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5]*3, [0.5]*3)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5]*3, [0.5]*3)
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5]*3, [0.5]*3)
+    ])
+}
